6d674cd5cd4b470b3efc51efe7e43586731c07d8
mohammad <mohammadakhlaghi843@gmail.com>
2024-10-10 17:53:18 +0200

    show all articles and add ckEditor5 to project and creating new Article handled

--- frontend/src/pages/AdminPanel/Articles/AdminArticles.jsx
+++ frontend/src/pages/AdminPanel/Articles/AdminArticles.jsx
@@ -1,24 +1,304 @@
-import { useEffect, useState } from 'react'
-import DataTable from '../../../Components/AdminPanel/DataTable/DataTable'
+import { useEffect, useState } from "react";
+import DataTable from "../../../Components/AdminPanel/DataTable/DataTable";
+import Swal from "sweetalert2";
+import { useForm } from "../../../hooks/useForm";
+import Input from "../../../components/Form/Input";
+import { minValidator } from "../../../components/validators/rules";
+import withReactContent from "sweetalert2-react-content";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import {
+  ClassicEditor,
+  Bold,
+  Essentials,
+  Italic,
+  Mention,
+  Paragraph,
+  Undo,
+  Heading,
+  Link,
+  Image,
+  ImageUpload,
+  BlockQuote,
+  List,
+} from "ckeditor5";
+
+import "ckeditor5/ckeditor5.css";
+import "ckeditor5-premium-features/ckeditor5-premium-features.css";
 
 export default function Articles() {
+  const [articles, setArticles] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [articleCategory, setArticleCategory] = useState("-1");
+  const [articleCover, setArticleCover] = useState({});
+  const [articleBody,setArticleBody ] = useState("");
 
-  const [articles, setArticles] = useState([])
+  const [formState, onInputHandler] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      shortName: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+  const mySwal = withReactContent(Swal);
 
   useEffect(() => {
-    fetch('http://localhost:4000/v1/articles')
-      .then(res => res.json())
-      .then(allArticles => {
+    getAllArticles();
+
+    fetch(`http://localhost:4000/v1/category`)
+      .then((res) => res.json())
+      .then((allCategories) => {
+        setCategories(allCategories);
+      });
+  }, []);
+
+  function getAllArticles() {
+    fetch("http://localhost:4000/v1/articles")
+      .then((res) => res.json())
+      .then((allArticles) => {
         console.log(allArticles);
-        setArticles(allArticles)
+        setArticles(allArticles);
+      });
+  }
+
+  const removeArticle = (articleID) => {
+    const localStorageDate = JSON.parse(localStorage.getItem("user"));
+    mySwal
+      .fire({
+        title: "آیا از حذف مقاله اطمینان دارید؟`",
+        icon: "warning",
+        cancelButtonText: "خیر",
+        confirmButtonText: "بله",
+        showCancelButton: true,
       })
-  }, [])
+      .then((result) => {
+        if (result.isConfirmed) {
+          fetch(`http://localhost:4000/v1/articles/${articleID}`, {
+            method: "DELETE",
+            headers: {
+              Authorization: `Bearer ${localStorageDate.token}`,
+            },
+          }).then((res) => {
+            if (res.ok) {
+              mySwal
+                .fire({
+                  title: "مقاله مورد نظر با موفقیت حذف شد",
+                  icon: "success",
+                  confirmButtonText: "اوکی",
+                })
+                .then(() => {
+                  getAllArticles();
+                });
+            }
+          });
+        }
+      });
+  };
+
+
+  const createNewArticle = (event) => {
+    event.preventDefault()
+    const localStorageData = JSON.parse(localStorage.getItem('user'))
+    let formData = new FormData()
+    formData.append('title',formState.inputs.title.value)
+    formData.append('shortName',formState.inputs.shortName.value)
+    formData.append('description',formState.inputs.description.value)
+    formData.append('categoryID',articleCategory)
+    formData.append('cover',articleCover)
+    formData.append('body',articleBody)
+
+    fetch('http://localhost:4000/v1/articles',{
+      method:"POST",
+      headers:{
+        Authorization: `Bearer ${localStorageData.token}`
+      },
+      body: formData
+    }).then(res => {
+      if(res.ok){
+        mySwal.fire({
+          title:'مقاله با موفقیت افزوده شد',
+          icon:'success',
+          confirmButtonText:'خیلی هم عالی'
+        }).then(result => {
+          if(result.isConfirmed){
+            getAllArticles()
+          }
+        })
+      }
+    })
+  }
 
   return (
     <>
-      <DataTable title="مقاله‌ها"
-      >
-         <table className="table">
+      <div className="container-fluid" id="home-content">
+        <div className="container">
+          <div className="home-title">
+            <span>افزودن مقاله جدید</span>
+          </div>
+          <form className="form">
+            <div className="col-6">
+              <div className="name input">
+                <label className="input-title" style={{ display: "block" }}>
+                  عنوان
+                </label>
+                <Input
+                  element="input"
+                  type="text"
+                  id="title"
+                  onInputHandler={onInputHandler}
+                  validations={[minValidator(8)]}
+                />
+                <span className="error-message text-danger"></span>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="name input">
+                <label className="input-title" style={{ display: "block" }}>
+                  لینک
+                </label>
+                <Input
+                  element="input"
+                  type="text"
+                  id="shortName"
+                  onInputHandler={onInputHandler}
+                  validations={[minValidator(5)]}
+                />
+                <span className="error-message text-danger"></span>
+              </div>
+            </div>
+            
+            <div className="col-12">
+              <div className="name input">
+                <label className="input-title" style={{ display: "block" }}>
+                  چکیده
+                </label>
+                <Input
+                  className='col-12'
+                  element="textarea"
+                  type="text"
+                  id="description"
+                  onInputHandler={onInputHandler}
+                  validations={[minValidator(5)]}
+                />
+                <span className="error-message text-danger"></span>
+              </div>
+            </div>
+            <div className="col-12">
+              <CKEditor
+                editor={ClassicEditor}
+                data={articleBody}
+                config={{
+                  toolbar: {
+                    items: [
+                      "undo",
+                      "redo",
+                      "|",
+                      "bold",
+                      "italic",
+                      "heading",
+                      "link",
+                      "uploadImage",
+                      "blockQuote",
+                      "numberedList",
+                      "bulletedList"
+                    ],
+                  },
+                  plugins: [
+                    Bold,
+                    Essentials,
+                    Italic,
+                    Mention,
+                    Paragraph,
+                    Undo,
+                    Heading,
+                    Image,
+                    Link,
+                    ImageUpload,
+                    BlockQuote,
+                    List,
+                    
+                  ],
+                  heading: {
+                    options: [
+                      {
+                        model: "paragraph",
+                        title: "Paragraph",
+                        class: "ck-heading_paragraph",
+                      },
+                      {
+                        model: "heading1",
+                        view: "h1",
+                        title: "Heading 1",
+                        class: "ck-heading_heading1",
+                      },
+                      {
+                        model: "heading2",
+                        view: "h2",
+                        title: "Heading 2",
+                        class: "ck-heading_heading2",
+                      },
+                    ],
+                  },
+                }}
+                onChange={(event, editor) => {
+                  setArticleBody(editor.getData())
+                }}
+              />
+            </div>
+            <div className="col-6">
+              <div className="name input">
+                <label className="input-title" style={{ display: "block" }}>
+                  کاور
+                </label>
+                <input
+                  type="file"
+                  onChange={(event) => {
+                    setArticleCover(event.target.files[0]);
+                  }}
+                />
+                <span className="error-message text-danger"></span>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="name input">
+                <label className="input-title" style={{ display: "block" }}>
+                  دسته بندی
+                </label>
+                <select
+                  onChange={(event) => setArticleCategory(event.target.value)}
+                >
+                  <option value="-1">دسته بندی مقاله را انتخاب کنید،</option>
+                  {categories.map((category) => (
+                    <option key={category._id} value={category._id}>
+                      {category.title}
+                    </option>
+                  ))}
+                </select>
+                <span className="error-message text-danger"></span>
+              </div>
+            </div>
+            <div className="col-12">
+              <div className="bottom-form">
+                <div className="submit-btn">
+                  <input type="submit" value="افزودن" onClick={(event) => createNewArticle(event)}/>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <DataTable title="مقاله‌ها">
+        <table className="table">
           <thead>
             <tr>
               <th>شناسه</th>
@@ -37,10 +317,7 @@ export default function Articles() {
                 <td>{article.shortName}</td>
                 <td>{article.creator.name}</td>
                 <td>
-                  <button
-                    type="button"
-                    className="btn btn-primary edit-btn"
-                  >
+                  <button type="button" className="btn btn-primary edit-btn">
                     ویرایش
                   </button>
                 </td>
@@ -48,6 +325,7 @@ export default function Articles() {
                   <button
                     type="button"
                     className="btn btn-danger delete-btn"
+                    onClick={() => removeArticle(article._id)}
                   >
                     حذف
                   </button>
@@ -58,5 +336,5 @@ export default function Articles() {
         </table>
       </DataTable>
     </>
-  )
+  );
 }
